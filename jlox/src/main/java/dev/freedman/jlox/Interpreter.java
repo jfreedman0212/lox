566c473bf755e5289f6633b66f214a86ba59422f
Josh Freedman <joshfreedman@pm.me>
2022-10-24 20:50:02 -0400

    Remove unused import

--- jlox/src/main/java/dev/freedman/jlox/Interpreter.java
+++ jlox/src/main/java/dev/freedman/jlox/Interpreter.java
@@ -3,7 +3,6 @@ package dev.freedman.jlox;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 
 public class Interpreter {
     private final Map<String, Object> variables;
